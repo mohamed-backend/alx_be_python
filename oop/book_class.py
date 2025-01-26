26e5c0c3b66624a57e56c465c39e9b00b2d059e7
mikwall <charchaoui.mohamed77@gmail.com>
2025-01-26 22:47:16 +0100

    Create book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        # User-friendly string representation
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        # Official string representation (could recreate the object)
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
